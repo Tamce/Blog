dc859bc217568e2a3dc2b30551a54f03dd382a85
Tamce <tamce@outlook.com>
2017-06-15 02:00:17 +0800

    Database migrations and Basic Model definitions

---
+++ app/Support/Session.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Support;
+
+class Session
+{
+    static protected $segmentName = '__tmc_session_control';
+    static public function set($key, $value = null)
+    {
+        if ($value == null) {
+            unset($_SESSION[self::$segmentName][$key]);
+            return null;
+        }
+        $_SESSION[self::$segmentName][$key] = $value;
+        return $value;
+    }
+
+    static public function remove($key)
+    {
+        return self::set($key);
+    }
+
+    static public function removeAll()
+    {
+        $_SESSION[$segmentName] = [];
+    }
+
+    static public function get($key, $default = null)
+    {
+        return isset($_SESSION[self::$segmentName][$key]) ? $_SESSION[self::$segmentName][$key] : $default;
+    }
+
+    static public function destroy()
+    {
+        session_destroy();
+    }
+
+    static public function start()
+    {
+        session_start();
+        $_SESSION[$segmentName] = [];
+    }
+
+    static public function restart()
+    {
+        self::destory();
+        self::start();
+    }
+}
+
+Session::start();
--- app/User.php
+++ app/User.php
@@ -12,21 +12,21 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 {
     use Authenticatable, Authorizable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email',
+        'name'
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post', 'created_by');
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = password_hash($value, PASSWORD_BCRYPT);
+    }
 }
